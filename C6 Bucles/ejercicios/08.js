133e122e3456e385400db342c2729295e200dc38
FrutaDoesBanana <ronaldo-equi@hotmail.com>
2024-02-20 16:25:59 -0600

    Ejercicios del 01 al 13

--- C6 Bucles/ejercicios/08.js
+++ C6 Bucles/ejercicios/08.js
@@ -1,8 +1,15 @@
+const retornarPerimetro = require("../../C5 Fundamentos Javascript/ejercicios/30");
+
 function esPositivoOInferiorA10(a) {
    // La función recibe un número "a" por parámetro.
    // Retorna true si es positivo y menor que 10.
    // Retorna false en caso contrario.
    // Tu código:
+   if (a >= 0 && a < 10) {
+      return true;
+   } else {
+      return false;
+   }
 }
 
 module.exports = esPositivoOInferiorA10;
